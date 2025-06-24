62545cc059fd29bf3c2427ee2b6629dd8cd8db0d
rafay2-source <ar1215729@gmail.com>
2025-06-24 13:15:12 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int age;
+    string name;
+    float GPA;
+
+    cout<< "Enter the age = ";
+    cin>> age;
+
+    cout<< "Enter the Name: ";
+    cin>> name;
+
+    cout<< "Enter the GPA: ";
+    cin>> GPA;
+
+    cout<< "The Age is: " << age << endl;
+    cout<< "The Name is: " << name << endl;
+    cout<< "The GPA is: " << GPA << endl;
+
+    return 0;
+}
